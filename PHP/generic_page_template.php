9b7e45ae962714b0db11d6b307bc6f9fa93eb01f
hreilly <hannahreillyca@gmail.com>
2018-11-29 16:49:12 -0800

    Create generic_page_template.php

---
+++ PHP/generic_page_template.php
@@ -0,0 +1,21 @@
+<?php
+// Template Name: Customer Care Package
+/**
+ * The template to display pricing for various customer care packages.
+ *
+ * @package understrap
+ */
+
+get_header();
+$container   = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="full-width-bg-image" style="background-image: url(<?php the_post_thumbnail_url( "full" ); ?>)"></div>
+
+    <div class="main-content-container">
+
+    <!-- Content -->
+
+    </div>
+
+<?php get_footer(); ?>
