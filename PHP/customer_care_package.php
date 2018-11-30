77487b9b3294a3ae907bf4a274bbb235bfb149ce
hreilly <hannahreillyca@gmail.com>
2018-11-30 15:29:44 -0800

    Update customer_care_package.php

--- PHP/customer_care_package.php
+++ PHP/customer_care_package.php
@@ -14,17 +14,23 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
     <div class="main-content-container">
 
-    <!-- Call and define pricing tables -->
-    <?php 
+        <div class="vs-overview-text" style="padding-top: 80px;">
+            <h2>Granville Homes Care Packages</h2>
+            <p style="padding-bottom: 20px;">Taking care of your home is an important part of maintaining your warranty, retaining your home's value, and increasing the longevity of crucial systems. With Granville's care packages, our Customer Care team takes the hassle out of home maintenance. Purchase single services on our a la carte menu, or bundle your services in a care package for even more savings.</p>
+            <h3>Complete packages starting at $1,085*</h3>
+        </div>
 
-        $one_story_table = get_field( 'one_story_pricing' );
-        $two_story_table = get_field( 'two_story_pricing' );
-        $estates_table = get_field( 'estates_pricing' );
+        <!-- Call and define pricing tables -->
+        <?php 
 
-        echo '<div>';
-        echo '</div>';
+            $one_story_table = get_field( 'one_story_pricing' );
+            $two_story_table = get_field( 'two_story_pricing' );
+            $estates_table = get_field( 'estates_pricing' );
 
-    ?>
+            echo '<div>';
+            echo '</div>';
+
+        ?>
 
     </div>
 
