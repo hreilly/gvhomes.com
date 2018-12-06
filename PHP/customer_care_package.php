e9819236655fcfe0d1708a8daa7fffd7e7214df5
hreilly <hannahreillyca@gmail.com>
2018-12-06 13:40:18 -0800

    Update customer_care_package.php

--- PHP/customer_care_package.php
+++ PHP/customer_care_package.php
@@ -63,6 +63,24 @@ $container   = get_theme_mod( 'understrap_container_type' );
     text-align: center;
     text-transform: uppercase;
 }
+#collapseOneStory .custom-lots-table-headers div:nth-of-type(2):before {
+    content: '$1,245.00';
+    padding-right: 10px;
+    color: #b1b1b1;
+    text-decoration: line-through;
+}
+#collapseTwoStory .custom-lots-table-headers div:nth-of-type(2):before {
+    content: '$1,625.00';
+    padding-right: 10px;
+    color: #b1b1b1;
+    text-decoration: line-through;
+}
+#collapseEstates .custom-lots-table-headers div:nth-of-type(2):before {
+    content: '$1,895.00';
+    padding-right: 10px;
+    color: #b1b1b1;
+    text-decoration: line-through;
+}
 @media screen and (max-width:1080px) {
     .services-flex-item {
         width: 30%;
@@ -77,6 +95,8 @@ $container   = get_theme_mod( 'understrap_container_type' );
 <!-- Limited width content -->
 <div class="main-content-container">
 
+    <!-- Note: Prices here are set in the base price fields on the admin page. -->
+
     <div class="vs-overview-text" style="padding-top: 80px;">
         <h2>Granville Homes Care Packages</h2>
         <p>Taking care of your home is an important part of maintaining your warranty, retaining your home's value, and increasing the longevity of crucial systems. With Granville's care packages, our Customer Care team takes the hassle out of home maintenance. Purchase single services on our a la carte menu, or bundle your services for even more savings.</p>
@@ -89,7 +109,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
             <p>Includes cleaning debris off the roof and roof valleys, roof inspection, and resealing of roof jacks.</p>
             <div class="flex-item-price">
                 <h5>Starting at:</h5>
-                <h3>$359<span style="font-weight: 400;">*</span></h3>
+                <h3>$<?php echo get_field( 'roof_base_price' ); ?><span style="font-weight: 400;">*</span></h3>
             </div>
         </div>
         <div class="services-flex-item">
@@ -98,7 +118,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
             <p>Includes cleaning of rain gutters, adjusting pitch if necessary, sealing any leaks, and re-attaching gutters if necessary.</p>
             <div class="flex-item-price">
                 <h5>Starting at:</h5>
-                <h3>$269<span style="font-weight: 400;">*</span></h3>
+                <h3>$<?php echo get_field( 'rain_gutter_base_price' ); ?><span style="font-weight: 400;">*</span></h3>
             </div>
         </div>
         <div class="services-flex-item">
@@ -107,7 +127,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
             <p>Includes replacement of HVAC air filters, testing of HVAC low voltage system, and inspection, cleaning, and lubrication of HVAC equipment.</p>
             <div class="flex-item-price">
                 <h5>Starting at:</h5>
-                <h3>$239<span style="font-weight: 400;">/unit*</span></h3>
+                <h3>$<?php echo get_field( 'hvac_base_price' ); ?><span style="font-weight: 400;">/unit*</span></h3>
             </div>
         </div>
         <div class="services-flex-item">
@@ -116,7 +136,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
             <p>Includes cleaning of sewer lines, inspection of toilets and under sinks for leaks, and inspection of all faucets and fixtures.</p>
             <div class="flex-item-price">
                 <h5>Starting at:</h5>
-                <h3>$179<span style="font-weight: 400;">*</span></h3>
+                <h3>$<?php echo get_field( 'plumbing_base_price' ); ?><span style="font-weight: 400;">*</span></h3>
             </div>
         </div>
         <div class="services-flex-item">
@@ -125,7 +145,7 @@ $container   = get_theme_mod( 'understrap_container_type' );
             <p>Includes vinegar flush and cleaning to remove mineral build-up and ensure that system is running at full capacity.</p>
             <div class="flex-item-price">
                 <h5>Starting at:</h5>
-                <h3>$199<span style="font-weight: 400;">/unit*</span></h3>
+                <h3>$<?php echo get_field( 'tankless_base_price' ); ?><span style="font-weight: 400;">/unit*</span></h3>
             </div>
         </div>
     </div>
@@ -136,13 +156,15 @@ $container   = get_theme_mod( 'understrap_container_type' );
         <h3>Save up to <span style="font-weight: 700;">15%</span>, with packages starting at <span style="font-weight: 700;">$1,065</span><span style="font-weight: 400;">*</span></h3>
     </div>
 
+<!-- Sum prices below must be updated in the CSS :before elements -->
+
     <!-- Single Story Pricing Table -->
 
     <div class="card" style="max-width: 1024px; margin: auto;">
 
         <div class="card-header" id="headingOneStory">
             <h5 class="mb-0">
-            <button class="btn btn-link expanded" data-toggle="collapse" data-target="#collapseOneStory" aria-expanded="false" aria-controls="collapseOneStory" style="white-space: normal; color: #252425; text-decoration: none; width: 100%;">
+            <button class="btn btn-link expanded" data-toggle="collapse" data-target="#collapseOneStory" aria-expanded="true" aria-controls="collapseOneStory" style="white-space: normal; color: #252425; text-decoration: none; width: 100%;">
                 <div class="collapse-label">Single Story Traditional &amp; Canvas Homes&nbsp;</div>
             </button>
             </h5>
@@ -315,8 +337,16 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
     <!-- Disclaimer -->
 
-    <div class="custom-lots-disclaimer" style="padding: 20px;">*Price for single story Traditional and Canvas homes only. Please see appropriate pricing tables for two story and Estates plans. Costs subject to change at any time and may be affected by age and condition of home. Please contact Customer Care for full terms and conditions.</div>
+    <div class="custom-lots-disclaimer" style="padding: 20px; max-width: 1024px; margin: auto; text-align: center;">*Price for single story Traditional and Canvas homes only. Please see appropriate pricing tables for two story and Estates plans. Costs subject to change at any time and may be affected by age and condition of home. Please contact Customer Care for full terms and conditions.</div>
     <p>&nbsp;</p>
+
+    <div class="footer-cta-background">
+        <div class="footer-cta-content">
+            <div class="footer-cta-text">Have questions? Our team is here to help.</div>
+            <div class="footer-cta-button"><a href="/customer-care/#cust-care-pack-contact" class="subtle-button">Contact Customer Care</a></div>
+        </div>
+    </div>
+    <div style="height: 20px; background-color: #ffffff;"></div>
 
 </div>
 
