91deb6b3e6313ef7b8cb48f1efd5c0e687a49d63
hreilly <hannahreillyca@gmail.com>
2019-03-14 10:02:45 -0700

    Create available_models_acf_check.php

---
+++ PHP/Snippets/available_models_acf_check.php
@@ -0,0 +1,43 @@
+<!-- Check for available models -->
+
+<?php 
+
+$models = array(
+    'post_type' => 'communities',
+    'meta_query' => array(
+        array(
+            'key' => 'models_available',
+            'value' => '"' . get_the_ID() . '"',
+            'compare' => 'LIKE'
+        )
+    )
+);
+
+$the_query = new WP_Query( $models );
+
+?>
+
+<style>
+    .model-community::after{
+        content: ','
+    }
+    .model-community:last-of-type::after{
+        content: ' '
+    }
+</style>
+
+<?php if( $the_query->have_posts() ): ?>
+
+    <p style="text-align: center; font-weight: bold; font-style: italic;">Model Available at:
+
+    <?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+        <a href="<?php echo the_permalink(); ?>" class="model-community"><?php echo the_title(); ?></a>
+
+    <?php endwhile; ?>
+
+    </p>
+
+<?php endif; ?>
+
+<?php wp_reset_query(); ?>
